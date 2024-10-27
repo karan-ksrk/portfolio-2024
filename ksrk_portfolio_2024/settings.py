69e17b109f4f7d5ce3fa64aad3686b1d34c3c78c
unknown <karanksrk2000@outlook.com>
2024-10-27 17:39:26 +0530

    updated settings

--- ksrk_portfolio_2024/settings.py
+++ ksrk_portfolio_2024/settings.py
@@ -129,8 +129,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
