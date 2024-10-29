35c1638ad027106183d9e5efd8b0a4ea925c2d4f
unknown <karanksrk2000@outlook.com>
2024-10-29 21:28:25 +0530

    moved to sqlite

--- ksrk_portfolio_2024/settings.py
+++ ksrk_portfolio_2024/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
 # database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 
